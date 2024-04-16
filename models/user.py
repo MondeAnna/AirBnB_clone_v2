9e547da0f9a0467b101b40105203af31b9153791
MondeAnna <monde.anna@gmail.com>
2024-04-16 16:23:20 +0200

    model (user): convert attr to table columns

--- models/user.py
+++ models/user.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel, sa
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+
+    email = sa.Column(
+        "email",
+        sa.String(128),
+        nullable=False,
+    )
+
+    password = sa.Column(
+        "password",
+        sa.String(128),
+        nullable=False,
+    )
+
+    first_name = sa.Column(
+        "first_name",
+        sa.String(128),
+        nullable=False,
+    )
+
+    last_name = sa.Column(
+        "last_name",
+        sa.String(128),
+        nullable=False,
+    )
