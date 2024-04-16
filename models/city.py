bd6ee1a2ae12978f9e11eccf99b77d86cf4b6402
MondeAnna <monde.anna@gmail.com>
2024-04-16 14:38:11 +0200

    model (city): convert variables to db columns

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel, sa
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    name = sa.Column(
+        "name",
+        sa.String(128),
+        nullable=False,
+    )
+
+    state_id = sa.Column(
+        "state_id",
+        sa.String(60),
+        sa.ForeignKey("states.id"),
+        nullable=False,
+    )
