da2ebf8110a56e9f8e77eda388324cfa459d8cff
MondeAnna <monde.anna@gmail.com>
2024-04-17 09:50:02 +0200

    test (base): init model without kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -49,8 +49,7 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,33 +2,53 @@
 """ place holders """
 from models.base_model import BaseModel
 import unittest
-import datetime
+from datetime import datetime
+import string
 import json
 import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ place holders """
+    """place holders"""
 
-    def __init__(self, *args, **kwargs):
-        """ place holders """
-        super().__init__(*args, **kwargs)
+    ALLOWED_ID_CHARS = string.hexdigits.lower() + "-"
+
+    DISALLOWED_ID_CHARS = (
+        string.whitespace + string.ascii_uppercase + string.punctuation
+    ).replace("-", "")
 
     def setUp(self):
-        """ place holders """
-        self.name = 'BaseModel'
-        self.model = BaseModel
+        """place holders"""
+        self.name = "BaseModel"
+        self.model = BaseModel()
 
     # patch open at some point
     def tearDown(self):
         try:
-            os.remove('file.json')
+            os.remove("file.json")
         except:
             pass
 
+    def test_model_without_kwargs(self):
+        """focus on `id`, `created_at` and `updated_at`"""
+        id_type = type(self.model.id)
+        created_type = type(self.model.created_at)
+        updated_type = type(self.model.updated_at)
+        datetime_type = type(datetime.now())
+
+        self.assertEqual(id_type, str)
+        self.assertEqual(created_type, datetime)
+        self.assertEqual(updated_type, datetime_type)
+
+        for char in self.model.id:
+            self.assertTrue(char in self.ALLOWED_ID_CHARS)
+            self.assertTrue(char not in self.DISALLOWED_ID_CHARS)
+
+        self.assertEqual(self.model.created_at, self.model.updated_at)
+
     @unittest.skip
     def test_kwargs(self):
-        """ place holders """
+        """place holders"""
         copy = self.model.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is self.model)
@@ -36,7 +56,7 @@ class test_basemodel(unittest.TestCase):
 
     @unittest.skip
     def test_kwargs_int(self):
-        """ place holders """
+        """place holders"""
         copy = self.model.to_dict()
         copy.update({1: 2})
         with self.assertRaises(TypeError):
@@ -44,53 +64,55 @@ class test_basemodel(unittest.TestCase):
 
     @unittest.skip
     def test_save(self):
-        """ Testing save """
+        """Testing save"""
         self.model.save()
         key = self.name + "." + self.model.id
-        with open('file.json', 'r') as f:
+        with open("file.json", "r") as f:
             j = json.load(f)
             self.assertEqual(j[key], self.model.to_dict())
 
     @unittest.skip
     def test_str(self):
-        """ place holders """
-        self.assertEqual(str(self.model), '[{}] ({}) {}'.format(self.name, self.model.id,
-                         self.model.__dict__))
+        """place holders"""
+        self.assertEqual(
+            str(self.model),
+            "[{}] ({}) {}".format(self.name, self.model.id, self.model.__dict__),
+        )
 
     @unittest.skip
     def test_todict(self):
-        """ place holders """
+        """place holders"""
         n = self.model.to_dict()
         self.assertEqual(self.model.to_dict(), n)
 
     @unittest.skip
     def test_kwargs_none(self):
-        """ place holders """
+        """place holders"""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     @unittest.skip
     def test_kwargs_one(self):
-        """ place holders """
-        n = {'Name': 'test'}
+        """place holders"""
+        n = {"Name": "test"}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     @unittest.skip
     def test_id(self):
-        """ place holders """
+        """place holders"""
         id_type = type(self.model.id)
         self.assertEqual(id_type, str)
 
     @unittest.skip
     def test_created_at(self):
-        """ place holders """
+        """place holders"""
         self.assertEqual(type(self.model.created_at), datetime.datetime)
 
     @unittest.skip
     def test_updated_at(self):
-        """ place holders """
+        """place holders"""
         self.assertEqual(type(self.model.updated_at), datetime.datetime)
         n = self.model.to_dict()
         new = BaseModel(**n)
